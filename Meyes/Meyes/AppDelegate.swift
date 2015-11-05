66b8cfc8353e02cff642548b05049c52e5fcf6a5
Nan Kang <robert.sharpnk@gmail.com>
2015-11-04 20:47:25 -0400

    initial commit

---
+++ Meyes/Meyes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Meyes
+//
+//  Created by Nan Kang on 2015-11-04.
+//  Copyright © 2015 Nan Kang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
